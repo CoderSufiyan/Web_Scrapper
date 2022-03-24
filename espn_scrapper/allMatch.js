3298486eb5d38db5fe73d92a36a32e4a79d5ce28
CoderSufiyan <sufiyanandkhan@gmail.com>
2022-03-24 23:19:35 +0530

    new modifications

--- espn_scrapper/allMatch.js
+++ espn_scrapper/allMatch.js
@@ -1,38 +1,33 @@
-let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
-
 const request = require("request");
 const cheerio = require("cheerio");
+// const getScorecardObj = require("./scorecards");
 const {gifs} = require("./scorecards");
-
 function getAllMatch(url) {
     // console.log("from allMatch.js ",url);
     request(url, cb);
 }
-
-function cb(err, res, body){
-    if(err){
-        console.log("error", err);
-    }else{
-        extractAllMatchLink(body);
-    }
+function cb(err, res, body) {
+  if (err) {
+    console.error("error", err);
+  } else {
+    extractAllMatchLink(body);
+  }
 }
-
 function extractAllMatchLink(html){
-    
     let selecTool = cheerio.load(html);
     let scorecardElemArr = selecTool('a[data-hover="Scorecard"]');
-
+    console.log(scorecardElemArr.length);
+    //attr methods -> Method for getting all attributes and their values
     for (let i = 0; i < scorecardElemArr.length; i++){
         let scorecardLink = selecTool(scorecardElemArr[i]).attr("href");
-        console.log(i + 1 + ") " + scorecardLink);
+        // console.log(i + 1 + ") " + scorecardLink);
         let fullLink = "https://www.espncricinfo.com" + scorecardLink;
         // getScorecardObj.gifs(fullLink);
         gifs(fullLink);
-        // break;
+        break;
 
     }
 }
-
 module.exports = {
-    getAllMatch: getAllMatch,
+  getAllMatch: getAllMatch,
 };
