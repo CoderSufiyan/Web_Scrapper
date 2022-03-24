8f97771674ede1ad2913f942d4660512a37008a0
CoderSufiyan <sufiyanandkhan@gmail.com>
2022-03-25 01:19:58 +0530

    added inning.html and mofified scorecards

--- espn_scrapper/scorecards.js
+++ espn_scrapper/scorecards.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const { find } = require("cheerio/lib/api/traversing");
 
 function getInfoFromScorecard(url) {
 //   console.log("from scorecards.js ",url);
@@ -38,19 +39,58 @@ function getMatchDetails(html) {
       );
       console.log(matchResEle.text());
   //4. get team names
-  let teamNames = selecTool(".name-detail>.name-link");
+  let teamNameArr = selecTool(".name-detail>.name-link");
   // console.log(teamNames.text());
-
-  let team1 = selecTool(teamNames[0]).text();
-  let team2 = selecTool(teamNames[1]).text();
+  let team1 = selecTool(teamNameArr[0]).text();
+  let team2 = selecTool(teamNameArr[1]).text();
   console.log(team1);
   console.log(team2);
 
   //5. get innings 
 
-  let allBatsmenRows = selecTool(".table.batsman tbody>tr");
-  console.log(allBatsmenRows.text());
+  let allBatsmenTable = selecTool(".table.batsman tbody");
+  console.log("number of batsmen tables are ->   ",allBatsmenTable.length);
+  let htmlString = "";
+  let count = 0;
+  for (let i = 0; i < allBatsmenTable.length; i++) {
+    htmlString = htmlString + selecTool(allBatsmenTable[i]).html();
+    //Get the descendants(table rows ) of each element (table )
+    let allRows = selecTool(allBatsmenTable[i]).find("tr"); // -> data of batsmen + empty rows 
     
+    for (let i = 0; i < allRows.length; i++) {
+      //Check to see if any of the matched elements have the given className
+      let row = selecTool(allRows[i]);
+      let firstColmnOfRow = row.find("td")[0];
+      if (selecTool(firstColmnOfRow).hasClass("batsman-cell")) {
+        //will be getting valid data
+        // count++;
+        // console.log("inside " + count);
+        // name | runs | balls | 4's | 6's | sr
+        // for (let i = 0; i < 8; i++) {
+        //   if (i == 1 || i == 4) continue;
+        //   else {
+        //     console.log(selecTool(row.find("td")[i]).text());
+        //   }
+        // }
+        let playerName = selecTool(row.find("td")[0]).text();
+        // console.log(playerName);
+        let runs = selecTool(row.find("td")[2]).text();
+        let balls = selecTool(row.find("td")[3]).text();
+        let numberOf4 = selecTool(row.find("td")[5]).text();
+        let numberOf6 = selecTool(row.find("td")[6]).text();
+        let sr = selecTool(row.find("td")[7]).text();
+
+        console.log(
+          `playerName -> ${playerName} runsScored ->  ${runs} ballsPlayed ->  ${balls} numbOfFours -> ${numberOf4} numbOfSixes -> ${numberOf6}  strikeRate-> ${sr}`
+        );
+
+
+      }
+    }
+  }
+
+    
+  // console.log(htmlString);
 }
 //visit every scorecard and get info 
 module.exports = {
