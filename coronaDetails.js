309430156d1fb06497bb90519a577faa426390bf
CoderSufiyan <sufiyanandkhan@gmail.com>
2022-03-27 01:27:15 +0530

    chalk addition

--- coronaDetails.js
+++ coronaDetails.js
@@ -2,6 +2,7 @@ const request = require("request");
 //cheerio
 // Cheerio parses HTML and it traverses the html so that data can be manipulated according to user's needs
 const cheerio=require("cheerio");
+const chalk = require("chalk");
 request("https://www.worldometers.info/coronavirus/", cb);
 
 function cb(err, res, body) {
